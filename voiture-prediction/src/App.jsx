70258e4b54f1d6c1c73ccb44c8a4b4ea56c8d844
Mathieu Soussignan <contact.mathieu.soussignan@gmail.com>
2024-12-03 14:37:22 +0100

    Update App.jsx

--- voiture-prediction/src/App.jsx
+++ voiture-prediction/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import HomePage from './pages/HomePage';
 import NavBar from './pages/NavBar';
@@ -34,12 +35,49 @@ function App() {
         }}
       >
         <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
-        {/* Enlever temporairement framer-motion pour tester */}
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/predict" element={<PredictionForm />} />
-          <Route path="/visualize" element={<VisualizationPage />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, x: -100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <HomePage />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/predict"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, x: -100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <PredictionForm />
+                </motion.div>
+              }
+            />
+            <Route
+              path="/visualize"
+              element={
+                <motion.div
+                  initial={{ opacity: 0, x: -100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <VisualizationPage />
+                </motion.div>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
         <Footer />
       </Box>
     </ThemeProvider>
