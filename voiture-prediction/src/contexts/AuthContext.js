a8cdf5f43e9e0a0e87c5356f59de62510a052618
Mathieu Soussignan <contact.mathieu.soussignan@gmail.com>
2024-12-04 16:32:59 +0100

    Update

--- voiture-prediction/src/contexts/AuthContext.js
+++ voiture-prediction/src/contexts/AuthContext.js
@@ -1,16 +1,20 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+// Crée un contexte pour l'authentification
 export const AuthContext = createContext();
 
+// Fournisseur d'authentification
 export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // Vérifie si l'utilisateur est connecté en vérifiant la présence du token
   useEffect(() => {
     const token = localStorage.getItem('token');
     setIsLoggedIn(!!token);
   }, []);
 
+  // Fournir l'état d'authentification et la fonction de mise à jour
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       {children}
@@ -18,11 +22,18 @@ export function AuthProvider({ children }) {
   );
 }
 
+// Type de validation pour les props
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-// Hook personnalisé pour accéder au contexte
+// Hook personnalisé pour accéder au contexte d'authentification
 export function useAuth() {
-  return useContext(AuthContext);
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth doit être utilisé dans un AuthProvider');
+  }
+
+  return context;
 }
